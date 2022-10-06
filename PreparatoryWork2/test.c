2d6d282ce2eea4ae5537b0992e6d217ba4c6fa27
mmma <298985343@qq.com>
2022-10-06 16:31:10 +0800

    create PreparatoryWork3 and test the example code

--- PreparatoryWork2/test.c
+++ PreparatoryWork2/test.c
@@ -1,30 +1,15 @@
 #include<stdio.h>
-int globla_var=5;
-const int maxn=1001;
-int mul(int a,int b){
-	return a*b;
+int a = 0;
+int b = 0;
+int max(int a, int b) {
+    if(a >= b) {
+        return a;
+    } else {
+        return b;
+    } 
 }
-int main(){
-	int n;
-	scanf("%d",&n);
-	while(n <= 10){
-		scanf("%d",&n);
-	}
-	
-	float f = 1.1;
-	int trans = n + f;
-	printf("%d\n",trans);
-	
-	int res = mul(trans, n);
-	printf("%d\n",res);
-	
-	for(int i = 0; i < n; i++){
-		res = res + 10;
-		res = res / 2;
-		if(res <= 10 && res%2==1){
-			break;
-		}
-	}
-	printf("%d\n",res);
-	return 0;
+int main() {
+    scanf("%d %d", &a, &b);
+    printf("max is: %d\n", max(a, b));
+    return 0; 
 }
