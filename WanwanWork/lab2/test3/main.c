dddb248831c895168233afa25663b15bf5c41464
G15 ReqLi20 <545837202@qq.com>
2022-10-11 18:53:31 +0800

    10.11 lab2

---
+++ WanwanWork/lab2/test3/main.c
@@ -0,0 +1,19 @@
+int globla_var = 0;
+int mul(int a,int b){
+	return a*b;
+}
+int main(){
+	int n;
+    n=getint();
+    //while语句
+	while(n<=10){
+        n=getint();
+	}
+    //隐式转换
+	float f = 1.1;
+	globla_var = n + f; 
+    //函数调用
+    n = mul(n,globla_var);
+    putint(n);
+    putf("\n");
+    }
